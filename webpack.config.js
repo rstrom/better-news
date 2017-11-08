5dd9353e236d5d96219d09e80e736ae00f6f9c7a
Robbie Strom <robbie.strom@gmail.com>
2017-11-08 13:15:25 +0100

    Add webpack config; Hello World app

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  entry: ["babel-polyfill", path.resolve(__dirname, "./src/index.js")],
+  output: {
+    filename: "app.js",
+    path: path.resolve(__dirname, "public")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["react", "env"],
+            plugins: ["transform-object-rest-spread"]
+          }
+        }
+      }
+    ]
+  }
+};
